cf21e50ad927fd61e56ef6794f23451fe7d6615c
Jack Hitchcock <Jhitchc5@uncc.edu>
2019-03-25 03:06:19 +0000

    Add Delete Function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,6 +27,12 @@ class ArticlesController < ApplicationController
           else
               render 'edit'
           end
+  def destroy
+     @article = Article.find(params[:id])
+     @article.destroy
+     redirect_to articles_path
+  end
+              
       end
 
   end
