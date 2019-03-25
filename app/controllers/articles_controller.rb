206f8c5e6dfe50fc1623e89f2f8adec1fcb1649f
Jack Hitchcock <Jhitchc5@uncc.edu>
2019-03-25 01:31:20 +0000

    List all articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,16 +2,21 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end
+    def index
+        @articles = Article.all
+    end
     def new
+        @article = Article.new
     end
-    def create
-        @article = Article.new(params[:article_params])
-        
-        @article.save
-        redirect_to @article
-    end
+  def create
+      @article = Article.new(params.require(:article).permit(:title, :text))
+      
+      @article.save
+      redirect_to @article
+  end
+
 end
 private
-def article_params
-    params.require(:article).permit(:title, :text)
-end
+    def article_params
+        params.require(:article).permit(:title, :text)
+    end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_18_005943) do
+ActiveRecord::Schema.define(version: 2019_03_19_144810) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
