9cdb014bc17c041c769b0c38649e57ee17abc620
Jack Hitchcock <Jhitchc5@uncc.edu>
2019-04-15 05:04:44 +0000

    Set up add_entry feature scenario 1

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,19 @@
 class CommentsController < ApplicationController
-    def create
+  def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
     redirect_to article_path(@article)
-    end
-    
-    private
+  end
+ 
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article)
+  end
+ 
+  private
     def comment_params
-        params.require(:comment).permit(:commenter, :body)
+      params.require(:comment).permit(:commenter, :body)
     end
 end
