2c680540559e2129f0d517bc3a1a0875ff166f83
Jack Hitchcock <Jhitchc5@uncc.edu>
2019-04-22 02:21:28 +0000

    Add destroy functionality for comments

--- spec/Integration/blogger_adds_an_article_spec.rb
+++ spec/Integration/blogger_adds_an_article_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper.rb'
 
-feature "Blogger adds an article" do
+feature"Blogger adds an article" do
     scenario "Blogger successfully navigates to the new articles page from the listing articles page" do
         visit articles_path
         expect(page).to have_content("Listing articles")
@@ -9,8 +9,7 @@ feature "Blogger adds an article" do
         expect(page).to have_field("Title")
         expect(page).to have_field("Text")
     end
-    
-    scenario "Blogger successfully creates a new article" do
+scenario "Blogger successfully creates a new article" do
         visit new_article_path
         expect(page).to have_content("New article")
         fill_in "Title", with: "New Capybara Article"
